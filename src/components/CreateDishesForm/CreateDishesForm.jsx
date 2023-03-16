7eb3dced78cd7a1f7f065094c8015cc730b02c2e
lvalcke <lvalcke88@gmail.com>
2023-03-16 02:25:10 -0500

    starting first components

---
+++ src/components/CreateDishesForm/CreateDishesForm.jsx
@@ -0,0 +1,9 @@
+const CreateDishesForm = () => {
+    return (
+        <div>
+            <h1>CreateDishesForm</h1>
+        </div>
+    )
+}
+
+export default CreateDishesForm
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App'
-import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import store from './redux/store/index'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <Provider store={store}>
@@ -14,4 +16,9 @@ root.render(
       <App />
     </BrowserRouter>
   </Provider>
-)
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
