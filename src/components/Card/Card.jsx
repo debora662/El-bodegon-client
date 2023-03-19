89397aa5fc3455cf0a95e2e7bf36d2b9aee54b8b
Nicolas González Rodríguez <nicogr300804@gmail.com>
2023-03-19 16:50:39 -0500

    Estilos 3.0

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -6,12 +6,12 @@ const Card = ({image, name, id, price, rating}) => {
   return (
     <div className={style.card}>
       {/* <p>Id: {id}</p> */}
-      <p>Name: {name}</p>
+      <p>{name}</p>
       <Link to ={`/detail/${id}`}>
         <img src={image} alt={name} className={style.card_image} />
       </Link>
-      <p>price: {price}</p>
-      <p>rating: {rating}</p>
+      <p>{price}</p>
+      <p>Rating {rating}</p>
       {/* <p>description: {props.description}</p>
       <p>category: {props.category}</p>
       <p>comments: {props.comments}</p> */}
