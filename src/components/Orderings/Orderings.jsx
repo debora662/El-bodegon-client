da132c7a9c94dfb7c3b13b6bb1a11f291a027e1f
LautyFarías247 <lauty0618@gmail.com>
2023-03-21 18:19:04 -0300

    BORRE CONSOLELOGS

--- src/components/Orderings/Orderings.jsx
+++ src/components/Orderings/Orderings.jsx
@@ -4,17 +4,15 @@ import { setOrderings } from "../../redux/actions/actions"
 const Orderings = ({setCurrentPage, setOrder}) => {
     const dispatch = useDispatch()
     const handleSort = (e) =>{
-        console.log("holaa");
         dispatch(setOrderings(e.target.value))
-        console.log(e.target.value);
         setCurrentPage(1)
         setOrder(`Ordered by ${e.target.value}`)
     }
     return (
         <select onChange={e=> handleSort(e)}>
             <option value="any">any</option>
-            <option value="Descendent price">Descendent price</option>
             <option value="Ascendent price">Ascendent price</option>
+            <option value="Descendent price">Descendent price</option>
             <option value="Ascendent rating">Ascendent rating</option>
             <option value="Descendent rating">Descendent rating</option>
         </select>
