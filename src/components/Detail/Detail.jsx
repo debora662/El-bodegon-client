f2c599088188a0439928eea36a402d9aa355b2ea
LautyFarías247 <lauty0618@gmail.com>
2023-03-22 01:28:43 -0300

    FUNCIONALIDAD DEL CARRITO

--- src/components/Detail/Detail.jsx
+++ src/components/Detail/Detail.jsx
@@ -5,18 +5,32 @@ import { Link } from "react-router-dom";
 
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { addProduct, getDishesById, removeProduct } from "../../redux/actions/actions";
+
 const Detail = () => {
   const { id } = useParams();
-  const [detailFood, setDetailFood] = useState();
-
+  const detailFood = useSelector(state => state.detail)
+  const dispatch = useDispatch()
+  const cart = useSelector(state => state.cart)
+  const quantity = cart.find(item => item._id === id)
+  const [aux, setAux] = useState("")
   useEffect(() => {
-    axios
-      .get(`https://el-bodegon-api-ochre.vercel.app/foods/${id}`)
-      .then((response) => setDetailFood(response.data)) 
-      .catch((error) => console.error(error));
-    
+    dispatch(getDishesById(id))  
   }, []);
+
+
+  const handleAddProduct = () =>{
+    setAux(aux + 1);
+    dispatch(addProduct(detailFood));
+    console.log(cart);
+  }
   
+  const handleRemoveProduct = () =>{
+    setAux (aux - 1);
+    dispatch(removeProduct(detailFood));
+    console.log(cart);
+  }
 
   return (
     <div className={style.detail}>
@@ -25,7 +39,7 @@ const Detail = () => {
           {/* <p>ID: {detailFood._id}</p> */}
           <p>{detailFood.name}</p>
 
-          <img src={detailFood.image.url? detailFood.image.url : detailFood.image } alt={detailFood.name} className={style.detail_img} />
+          <img src={detailFood.image?.url? detailFood.image?.url : detailFood.image } alt={detailFood.name} className={style.detail_img} />
           <p>${detailFood.price} USD</p>
           <p>{detailFood.description}</p>
           <p>{detailFood.category}</p>
@@ -33,6 +47,13 @@ const Detail = () => {
           <p>Comments: {detailFood.comments}</p>
         </div>
       )}
+
+      <div>
+        <button onClick={handleRemoveProduct}>-</button>
+        <span>{quantity ? quantity.quantity : 0}</span>
+        <button onClick={handleAddProduct}>+</button>
+      </div>
+
           <Link to={"/menu"}>
             <button className={style.detailButton}>Volver</button>
           </Link>
