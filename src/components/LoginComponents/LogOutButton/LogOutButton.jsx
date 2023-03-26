990e1084c57c7c2dee9462e4df2087694ab96b9a
Nicolas González Rodríguez <nicogr300804@gmail.com>
2023-03-25 19:14:10 -0500

    correcciones

--- src/components/LoginComponents/LogOutButton/LogOutButton.jsx
+++ src/components/LoginComponents/LogOutButton/LogOutButton.jsx
@@ -1,5 +1,6 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import style from "./LogoutButton.module.css"
+import {FiLogOut} from "react-icons/fi"
 
 const LogoutButton = () => {
   const { logout } = useAuth0();
@@ -7,6 +8,7 @@ const LogoutButton = () => {
   return (
     <div>
       <button className={style.logout} onClick={logout}>Log Out</button>
+      <FiLogOut/>
     </div>
   );
 };
